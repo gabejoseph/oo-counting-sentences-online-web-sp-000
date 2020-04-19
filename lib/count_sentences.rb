077ae0b0e36e2cb934674d5f65b4cc2f20979c85
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-19 18:54:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      return false 
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      return false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false 
+    end  
   end
 
   def count_sentences
-
+    binding.pry 
+    if self.split('.') > 0 
+      return self.split('.').count
+    else 
+      return 0 
+    end
   end
 end
